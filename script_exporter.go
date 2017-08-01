6faa4c1345b8ec5a78eaebca4e87ab278333d609
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2017-08-01 18:47:49 +0700

    Add /metrics and /probe to execute script config

--- script_exporter.go
+++ script_exporter.go
@@ -1,4 +1,3 @@
-// TODO: load script metrics into prometheus
 // TODO: run script
 // TODO: add timeout when running script (with context)
 // TODO: run script in parallel
@@ -7,10 +6,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -30,6 +32,21 @@ var (
 	scriptsConfig = flag.String("scripts_config", "./scripts_config.yml", "scripts configuration path")
 )
 
+func probeHandler(w http.ResponseWriter, r *http.Request, conf []Script) {
+	probeSuccessGauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "script_success",
+		Help: "Script success",
+	}, []string{"script"})
+
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(probeSuccessGauge)
+
+	probeSuccessGauge.With(prometheus.Labels{"script": "ping"}).Set(1.0)
+
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
 func main() {
 	flag.Parse()
 
@@ -41,5 +58,9 @@ func main() {
 	var conf Config
 	yaml.Unmarshal(b, &conf)
 
-	fmt.Println(conf)
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
+		probeHandler(w, r, conf.ScriptsConfig)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
